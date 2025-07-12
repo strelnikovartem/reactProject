c788138190685743782ea958ea77baa69a24386e
Artem Strelnikov <piramida1237@gmail.com>
2025-07-12 20:00:26 +0200

    ggh

--- js/react.js
+++ js/react.js
@@ -1,1 +1,9 @@
-console.log('ggg');
+const user = {
+  username: 'Victor',
+  showName() {
+    // ❌ Віктор біжить швидко, тому що Віктор намагається зловити поїзд
+    console.log(this.username);
+  },
+};
+
+user.showName();

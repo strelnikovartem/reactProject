23a96c4f221abd0a0365b22f73bdb39ba117132c
Artem Strelnikov <piramida1237@gmail.com>
2025-07-13 08:25:17 +0200

    sxdc

--- js/react.js
+++ js/react.js
@@ -4,14 +4,5 @@ function showThis() {
   console.log('this in showThis: ', this);
 }
 
-const user = {
-  username: 'Poly',
-};
-
-user.showContext = showThis;
-
-// Викликаємо в контексті об'єкта
-user.showContext(); // this in showThis: {username: "Poly", showContext: ƒ}
-
-// Викликаємо в глобальному контексті
+// Викликаємо у глобальному контексті
 showThis(); // "this in showThis: undefined"

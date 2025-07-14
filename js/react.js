12acc437cc9a764c9f0f756d7f41946e7fce52ee
Artem Strelnikov <piramida1237@gmail.com>
2025-07-14 20:17:41 +0200

    dfvgbgg

--- js/react.js
+++ js/react.js
@@ -2,8 +2,8 @@ const baseSalary = 30000;
 const overtime = 10;
 const rate = 20;
 
-const getWage = (baseSalary, overtime, rate) => {
-  return baseSalary + overtime * rate;
+const getWage = (base, overtime, rate) => {
+  return base + overtime * rate;
 };
 
 console.log(getWage(baseSalary, overtime, rate));

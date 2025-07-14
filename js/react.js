88a93104eb44fee574478b6b0492308e77fc9b5f
Artem Strelnikov <piramida1237@gmail.com>
2025-07-14 20:19:31 +0200

    gbhn

--- js/react.js
+++ js/react.js
@@ -1,9 +1,10 @@
-const baseSalary = 30000;
-const overtime = 10;
-const rate = 20;
-
-const getWage = (base, overtime, rate) => {
-  return base + overtime * rate;
+const employee = {
+  baseSalary: 30000,
+  overtime: 10,
+  rate: 20,
+  getWage() {
+    return this.baseSalary + this.overtime * this.rate;
+  },
 };
 
-console.log(getWage(baseSalary, overtime, rate));
+console.log(employee.getWage());

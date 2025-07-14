d247f9903595e7d6e93bb913e711d659dd8bf61a
Artem Strelnikov <piramida1237@gmail.com>
2025-07-14 16:11:05 +0200

    vghjkm

--- js/react.js
+++ js/react.js
@@ -1,8 +1,10 @@
-const animal = {
-  legs: 4,
-};
-
+const animal = { legs: 4 };
 const dog = Object.create(animal);
 dog.name = 'Mango';
 
-console.log(dog); // { name: "Mango", [[Prototype]]: animal }
+console.log(Object.keys(dog)); // ["name"]
+console.log(Object.values(dog)); // ["Mango"]
+
+for (const key of Object.keys(dog)) {
+  console.log(key); // "name"
+}

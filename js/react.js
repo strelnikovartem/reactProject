c92011f6dd77c731dc090fd48ac3e36fb57f248c
Artem Strelnikov <piramida1237@gmail.com>
2025-07-15 16:49:45 +0200

    dcfvgb

--- js/react.js
+++ js/react.js
@@ -1,8 +1,11 @@
 class User {
-  constructor() {
-    console.log('constructor call');
+  constructor(name, email) {
+    console.log(name, email);
   }
 }
 
-const mango = new User(); // "constructor call"
-console.log(mango); // {}
+const mango = new User('Mango', 'mango@mail.com'); // "Mango mango@mail.com"
+// console.log(mango); // {}
+
+const poly = new User('poly', 'fojkk');
+console.log(poly);

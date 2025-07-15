f85e2e6938d631888e8267948b62c5626e655ccc
Artem Strelnikov <piramida1237@gmail.com>
2025-07-15 17:20:05 +0200

    aazsx

--- js/react.js
+++ js/react.js
@@ -12,3 +12,24 @@ class User {
     this.email = newEmail;
   }
 }
+
+const mango = new User({
+  name: 'Mango',
+  email: 'mango@mail.com',
+});
+
+console.log(mango.getEmail()); // "mango@mail.com"
+
+mango.changeEmail('new@mail.com');
+
+console.log(mango.getEmail()); // "new@mail.com"
+
+const poly = new User({
+  name: 'Mao',
+  email: 'mano@mail.com',
+});
+
+poly.changeEmail('ggg');
+
+console.log(poly);
+console.log(poly.getEmail());

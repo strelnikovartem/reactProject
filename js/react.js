e8bd8af3f6c1c15cb024d97b616bc40010d4d920
Artem Strelnikov <piramida1237@gmail.com>
2025-07-18 23:55:38 +0200

    sxdcf

--- js/react.js
+++ js/react.js
@@ -1,37 +1,5 @@
-class User {
-  #email;
-
-  constructor(params) {
-    this.name = params.name;
-    this.#email = params.email;
-  }
-
-  // Геттер email
-  get email() {
-    return this.#email;
-  }
-
-  // Сеттер email
-  set email(newEmail) {
-    this.#email = newEmail;
-  }
-  set email(newEmail) {
-    if (newEmail === '') {
-      console.log('Помилка! Пошта не може бути порожнім рядком!');
-      return;
-    }
-
-    this.#email = newEmail;
-  }
+class MyClass {
+  static myProp = 'value';
 }
 
-const mango = new User({
-  name: 'Mango',
-  email: 'mango@mail.com',
-});
-
-console.log(mango.email); // mango@mail.com
-
-mango.email = 'mango@supermail.com';
-
-console.log(mango.email); // mango@supermail.com
+console.log(MyClass.myProp); // "value"

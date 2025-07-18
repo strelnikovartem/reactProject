7c7846cef333fa1c6e041b70a774ab1362db10a9
Artem Strelnikov <piramida1237@gmail.com>
2025-07-18 23:46:51 +0200

    xsds

--- js/react.js
+++ js/react.js
@@ -1,5 +1,4 @@
 class User {
-  name;
   #email;
 
   constructor(params) {
@@ -7,35 +6,13 @@ class User {
     this.#email = params.email;
   }
 
-  // Публічний метод для отримання електронної пошти
-  getEmail() {
+  // Геттер email
+  get email() {
     return this.#email;
   }
 
-  // Публічний метод для зміни електронної пошти
-  changeEmail(newEmail) {
-    if (this.#validateEmail(newEmail)) {
-      this.#email = newEmail;
-    } else {
-      console.log('Invalid email format');
-    }
-  }
-
-  // Приватний метод для валідації електронної пошти
-  #validateEmail(email) {
-    return email.includes('@');
+  // Сеттер email
+  set email(newEmail) {
+    this.#email = newEmail;
   }
 }
-
-const mango = new User({
-  name: 'Mango',
-  email: 'mango@mail.com',
-});
-
-// Спробуємо змінити електронну пошту
-mango.changeEmail('newmail.com'); // "Invalid email format"
-mango.changeEmail('new@mail.com');
-console.log(mango.getEmail()); // "new@mail.com"
-
-// Прямий виклик приватного методу ззовні призведе до помилки
-// mango.#validateEmail('test'); // Помилка

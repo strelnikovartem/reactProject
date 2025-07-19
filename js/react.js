65397bb7bfc648bce03b0ed2bc6de12857875be5
Artem Strelnikov <piramida1237@gmail.com>
2025-07-19 14:19:27 +0200

    xdcfvs

--- js/react.js
+++ js/react.js
@@ -1,3 +1,29 @@
+class Car {
+  static maxPrice = 50000;
+  #price;
+
+  constructor(params) {
+    this.#price = params.price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    this.#price = newPrice;
+  }
+}
+
+const audi = new Car({ price: 35000 });
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
+
 class User {
   static roles = {
     admin: 'admin',

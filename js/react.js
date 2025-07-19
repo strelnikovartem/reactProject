93479675f800a7345f8f8c87fc67843f79582645
Artem Strelnikov <piramida1237@gmail.com>
2025-07-19 14:11:55 +0200

    zxc

--- js/react.js
+++ js/react.js
@@ -1,32 +1,6 @@
-class User {
-  static roles = {
-    admin: 'admin',
-    editor: 'editor',
-    basic: 'basic',
-  };
-
-  #email;
-  #role;
-
-  constructor(params) {
-    this.#email = params.email;
-    this.#role = params.role || User.roles.basic;
-  }
-
-  get role() {
-    return this.#role;
-  }
-
-  set role(newRole) {
-    this.#role = newRole;
-  }
+class MyClass {
+  static myProp = 'value';
 }
 
-const mango = new User({
-  email: 'mango@mail.com',
-  role: User.roles.admin,
-});
-
-console.log(mango.role); // "admin"
-mango.role = User.roles.editor;
-console.log(mango.role); // "editor"
+const inst = new MyClass();
+console.log(inst.myProp); // undefined

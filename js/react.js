e9bfae832f56875180b5351c57c620737d7130e1
Artem Strelnikov <piramida1237@gmail.com>
2025-07-19 14:11:10 +0200

    dcfvf

--- js/react.js
+++ js/react.js
@@ -1,5 +1,32 @@
-class MyClass {
-  static myProp = 'value';
+class User {
+  static roles = {
+    admin: 'admin',
+    editor: 'editor',
+    basic: 'basic',
+  };
+
+  #email;
+  #role;
+
+  constructor(params) {
+    this.#email = params.email;
+    this.#role = params.role || User.roles.basic;
+  }
+
+  get role() {
+    return this.#role;
+  }
+
+  set role(newRole) {
+    this.#role = newRole;
+  }
 }
 
-console.log(MyClass.myProp); // "value"
+const mango = new User({
+  email: 'mango@mail.com',
+  role: User.roles.admin,
+});
+
+console.log(mango.role); // "admin"
+mango.role = User.roles.editor;
+console.log(mango.role); // "editor"

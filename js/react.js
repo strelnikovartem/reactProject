eea4f35e6a410911a03d7ea1531cd2afa2c9383c
Artem Strelnikov <piramida1237@gmail.com>
2025-07-19 14:15:37 +0200

    ;l;l

--- js/react.js
+++ js/react.js
@@ -1,6 +1,32 @@
-class MyClass {
-  static myProp = 'value';
+class User {
+  static roles = {
+    admin: 'admin',
+    editor: 'editor',
+    basic: 'basic',
+  };
+
+  #email;
+  #role;
+
+  constructor(params) {
+    this.#email = params.email;
+    this.#role = params.role || User.roles.basic;
+  }
+
+  get role() {
+    return this.#role;
+  }
+
+  set role(newRole) {
+    this.#role = newRole;
+  }
 }
 
-const inst = new MyClass();
-console.log(inst.myProp); // undefined
+const mango = new User({
+  email: 'mango@mail.com',
+  role: User.roles.admin,
+});
+
+console.log(mango.role); // "admin"
+mango.role = User.roles.editor;
+console.log(mango.role); // "editor"

99f76488200ef9cb1fe16cddacc465094ffec5b8
Artem Strelnikov <piramida1237@gmail.com>
2025-07-19 14:31:34 +0200

    lkm

--- js/react.js
+++ js/react.js
@@ -12,7 +12,7 @@ class Car {
 
   set price(newPrice) {
     if (newPrice <= this.maxPrice) {
-      return (this.#price = newPrice);
+      this.#price = newPrice;
     }
   }
 }
@@ -25,36 +25,3 @@ console.log(audi.price); // 49000
 
 audi.price = 51000;
 console.log(audi.price); // 49000
-
-class User {
-  static roles = {
-    admin: 'admin',
-    editor: 'editor',
-    basic: 'basic',
-  };
-
-  #email;
-  #role;
-
-  constructor(params) {
-    this.#email = params.email;
-    this.#role = params.role || User.roles.basic;
-  }
-
-  get role() {
-    return this.#role;
-  }
-
-  set role(newRole) {
-    this.#role = newRole;
-  }
-}
-
-const mango = new User({
-  email: 'mango@mail.com',
-  role: User.roles.admin,
-});
-
-console.log(mango.role); // "admin"
-mango.role = User.roles.editor;
-console.log(mango.role); // "editor"

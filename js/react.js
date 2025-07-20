f4b01a02afd8071d3c5ebcac9b2b17505f472ed7
Artem Strelnikov <piramida1237@gmail.com>
2025-07-20 13:10:02 +0200

    dgftgy

--- js/react.js
+++ js/react.js
@@ -13,6 +13,6 @@ const bmw = new Car({ brend: 'bmw', model: 'x5', price: '50' });
 const audi = new Car({ brend: 'audi', model: 'q7', price: '100' });
 const lada = new Car({ brend: 'lada', model: 'pri', price: '10' });
 
-console.log(bmw);
-console.log(audi);
+console.log(bmw.getModel());
+console.log(audi.getModel());
 console.log(lada);

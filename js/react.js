64b81a035d6d1c6986253bd7c59fe4e6b78fc1e4
Artem Strelnikov <piramida1237@gmail.com>
2025-07-20 11:43:52 +0200

    bmw

--- js/react.js
+++ js/react.js
@@ -1,49 +1,9 @@
-class User {
-  email;
-
-  constructor(email) {
-    this.email = email;
-  }
-
-  get email() {
-    return this.email;
-  }
-
-  set email(newEmail) {
-    this.email = newEmail;
+class Car {
+  constructor() {
+    this.brend = 'win';
   }
 }
 
-class Admin extends User {
-  static role = {
-    BASIC: 'basic',
-    SUPERUSER: 'superuser',
-  };
-}
+const bmw = new Car();
 
-const mango = new Admin({
-  email: 'mango@mail.com',
-  access: Admin.role.SUPERUSER,
-});
-
-console.log(mango.email); // "mango@mail.com"
-console.log(mango.access); // "superuser"
-
-class Admin extends User {
-  constructor(params) {
-    super(params.email);
-    this.posts = params.posts;
-  }
-  static role = {
-    BASIC: 'basic',
-    SUPERUSER: 'superuser',
-  };
-}
-
-const mango = new Admin({
-  email: 'mango@mail.com',
-  access: Admin.role.SUPERUSER,
-});
-
-console.log(mango.email); // "mango@mail.com"
-console.log(mango.access); // "superuser"
+console.log(bmw);

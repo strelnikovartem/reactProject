038a46a9533f353f838855d38f83f4c2fa8cab12
Artem Strelnikov <piramida1237@gmail.com>
2025-07-20 13:13:19 +0200

    kmkml

--- js/react.js
+++ js/react.js
@@ -16,6 +16,8 @@ const bmw = new Car({ brend: 'bmw', model: 'x5', price: '50' });
 const audi = new Car({ brend: 'audi', model: 'q7', price: '100' });
 const lada = new Car({ brend: 'lada', model: 'pri', price: '10' });
 
+bmw.changeModel('BmW');
+
 console.log(bmw.getModel());
 console.log(audi.getModel());
 console.log(lada);

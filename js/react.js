a80d5d952a29dd21dd0ba4130d6abf19446fceb4
Artem Strelnikov <piramida1237@gmail.com>
2025-07-24 10:29:57 +0200

    jjj

--- js/react.js
+++ js/react.js
@@ -1,3 +1,6 @@
 const link = document.querySelector('.link');
 console.log(link.classList);
 // ["link", "is-active", length: 2, value: "link is-active"]
+
+const hasActiveClass = link.classList.contains('is-active'); // true
+const hasActiveClass = link.classList.contains('title'); // false

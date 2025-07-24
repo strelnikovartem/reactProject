097b03b4ab4d4a3e4567606fb72cadb7701e25b0
Artem Strelnikov <piramida1237@gmail.com>
2025-07-24 10:31:27 +0200

    dcfvg

--- js/react.js
+++ js/react.js
@@ -2,5 +2,7 @@ const link = document.querySelector('.link');
 console.log(link.classList);
 // ["link", "is-active", length: 2, value: "link is-active"]
 
-const hasActiveClass = link.classList.contains('is-active'); // true
+// const hasActiveClass = link.classList.contains('is-active'); // true
 const hasActiveClass = link.classList.contains('title'); // false
+
+console.log(hasActiveClass);

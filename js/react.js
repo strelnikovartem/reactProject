8009ae36e9c1aea513dace027f86909a6a5f6dd4
Artem Strelnikov <piramida1237@gmail.com>
2025-07-24 12:34:39 +0200

    www

--- js/react.js
+++
@@ -1,9 +0,0 @@
-const heading = document.createElement('h1');
-heading.classList.add('title');
-heading.textContent = 'This is a heading';
-console.log(heading); // <h1 class="title">This is a heading</h1>
-
-const image = document.createElement('img');
-image.src = 'https://picsum.photos/id/11/320/240';
-image.alt = 'Nature';
-console.log(image); // <img src="https://picsum.photos/id/11/320/240" alt="Nature" />

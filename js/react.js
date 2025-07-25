4b3536f4c40c3f1ee3f7836ea63162d2c91bc478
Artem Strelnikov <piramida1237@gmail.com>
2025-07-25 10:03:30 +0200

    dfvff

--- js/react.js
+++ js/react.js
@@ -1,11 +1,1 @@
-const list = document.querySelector('.usernames');
 
-// Adds an item to the end of the list
-const lastItem = document.createElement('li');
-lastItem.textContent = 'Poly';
-list.append(lastItem);
-
-// Adds an item to the beginning of the list
-const firstItem = document.createElement('li');
-firstItem.textContent = 'Ajax';
-list.prepend(firstItem);

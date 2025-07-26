dab056141079097f9391bc1e5cf61312ed9b5739
Artem Strelnikov <piramida1237@gmail.com>
2025-07-26 14:05:39 +0200

    dcdfd

--- js/react.js
+++ js/react.js
@@ -5,3 +5,19 @@ const handleClick = () => {
 };
 
 singleBtn.addEventListener('click', handleClick);
+
+const multiBtn = document.querySelector('#multiple');
+
+const firstCallback = () => {
+  console.log('First callback!');
+};
+const secondCallback = () => {
+  console.log('Second callback!');
+};
+const thirdCallback = () => {
+  console.log('Third callback!');
+};
+
+multiBtn.addEventListener('click', firstCallback);
+multiBtn.addEventListener('click', secondCallback);
+multiBtn.addEventListener('click', thirdCallback);

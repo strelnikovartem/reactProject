25bcc32d22de6e3609ac5f7fb1315264a8ca3102
Artem Strelnikov <piramida1237@gmail.com>
2025-07-26 14:10:15 +0200

    dcxcd

--- js/react.js
+++ js/react.js
@@ -1,23 +1,17 @@
-const singleBtn = document.querySelector('#single');
+const addListenerBtn = document.querySelector('.js-add');
+const removeListenerBtn = document.querySelector('.js-remove');
+const btn = document.querySelector('.target-btn');
 
 const handleClick = () => {
   console.log('click event listener callback');
 };
 
-singleBtn.addEventListener('click', handleClick);
+addListenerBtn.addEventListener('click', () => {
+  btn.addEventListener('click', handleClick);
+  console.log('click event listener was added to btn');
+});
 
-const multiBtn = document.querySelector('#multiple');
-
-const firstCallback = () => {
-  console.log('First callback!');
-};
-const secondCallback = () => {
-  console.log('Second callback!');
-};
-const thirdCallback = () => {
-  console.log('Third callback!');
-};
-
-multiBtn.addEventListener('click', firstCallback);
-multiBtn.addEventListener('click', secondCallback);
-multiBtn.addEventListener('click', thirdCallback);
+removeListenerBtn.addEventListener('click', () => {
+  btn.removeEventListener('click', handleClick);
+  console.log('click event listener was removed from btn');
+});

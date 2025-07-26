3029057a7c52421289bb31f4bbe2b4f3450838fb
Artem Strelnikov <piramida1237@gmail.com>
2025-07-26 14:46:05 +0200

    dcrfv

--- js/react.js
+++ js/react.js
@@ -1,17 +1,9 @@
-const addListenerBtn = document.querySelector('.js-add');
-const removeListenerBtn = document.querySelector('.js-remove');
-const btn = document.querySelector('.target-btn');
+const button = document.querySelector('.btn');
 
-const handleClick = () => {
-  console.log('click event listener callback');
+const handleClick = event => {
+  console.log('event: ', event);
+  console.log('event type: ', event.type);
+  console.log('currentTarget: ', event.currentTarget);
 };
 
-addListenerBtn.addEventListener('click', () => {
-  btn.addEventListener('click', handleClick);
-  console.log('click event listener was added to btn');
-});
-
-removeListenerBtn.addEventListener('click', () => {
-  btn.removeEventListener('click', handleClick);
-  console.log('click event listener was removed from btn');
-});
+button.addEventListener('click', handleClick);

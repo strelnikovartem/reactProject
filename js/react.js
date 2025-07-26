921d23ebcc05f71d8eb44d93ac112b5cc6924359
Artem Strelnikov <piramida1237@gmail.com>
2025-07-26 14:01:22 +0200

    defrd

--- js/react.js
+++ js/react.js
@@ -1,5 +1,7 @@
 const button = document.querySelector('.my-button');
 
-button.addEventListener('click', () => {
+const handleClick = () => {
   console.log('The button was pressed and now the next image will appear');
-});
+};
+
+button.addEventListener('click', handleClick);

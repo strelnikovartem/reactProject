ff6ec43d530ffb592547d01f45df9b58095e485a
Artem Strelnikov <piramida1237@gmail.com>
2025-07-26 12:54:48 +0200

    sxdc

--- js/react.js
+++ js/react.js
@@ -1,12 +1,5 @@
-const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
-const list = document.querySelector('.list');
+const button = document.querySelector('.my-button');
 
-const markup = technologies
-  .map(technology => `<li class="list-item">${technology}</li>`)
-  .join('');
-
-// Check the console, you'll see a single string with HTML tags
-console.log(markup);
-
-// Adding all the markup in one operation
-list.innerHTML = markup;
+button.addEventListener('click', () => {
+  console.log('The button was pressed and now the next image will appear');
+});

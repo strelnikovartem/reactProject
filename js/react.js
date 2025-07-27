361477a314cf378aadf1e4f54b7c16ecb5a15bd4
Artem Strelnikov <piramida1237@gmail.com>
2025-07-27 19:38:34 +0200

    submit

---
+++ js/react.js
@@ -0,0 +1,17 @@
+const registerForm = document.querySelector('.form');
+
+registerForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.login.value;
+  const password = form.elements.password.value;
+
+  if (login === '' || password === '') {
+    return console.log('Please fill in all the fields!');
+  }
+
+  console.log(`Login: ${login}, Password: ${password}`);
+  form.reset();
+}

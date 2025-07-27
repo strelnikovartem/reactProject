4076c763ab240b001db77542eb4ffefcec25babb
Artem Strelnikov <piramida1237@gmail.com>
2025-07-27 20:03:32 +0200

    lolpl

--- js/react.js
+++ js/react.js
@@ -1,18 +1,15 @@
-const registerForm = document.querySelector('.form');
+const select = document.querySelector('.pizza-select');
+const textOutput = document.querySelector('.text-output');
+const valueOutput = document.querySelector('.value-output');
 
-registerForm.addEventListener('submit', handleSubmit);
+select.addEventListener('change', setOutput);
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const form = event.target;
-  const login = form.elements.login.value;
-  const password = form.elements.password.value;
-  console.log(form);
+function setOutput(event) {
+  const selectedOptionValue = event.currentTarget.value;
+  const selectedOptionIndex = event.currentTarget.selectedIndex;
+  const selectedOptionText =
+    event.currentTarget.options[selectedOptionIndex].text;
 
-  if (login === '' || password === '') {
-    return console.log('Please fill in all the fields!');
-  }
-
-  console.log(`Login: ${login}, Password: ${password}`);
-  form.reset();
+  textOutput.textContent = selectedOptionText;
+  valueOutput.textContent = selectedOptionValue;
 }

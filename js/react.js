6eb1d291cf64373eb20c8431c0a27b70e2189b7f
Artem Strelnikov <piramida1237@gmail.com>
2025-07-30 11:04:03 +0200

    kklkl

---
+++ js/react.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.js-button');
+const box = document.querySelector('.js-box');
+
+btn.addEventListener('click', hendelClick);
+
+function hendelClick() {
+  console.log('ok');
+}

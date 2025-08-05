c11f39a54aa470f9c30f72fb6020c939666cb442
Artem Strelnikov <piramida1237@gmail.com>
2025-08-05 19:40:35 +0200

    finish_1

--- js/react.js
+++ js/react.js
@@ -1,16 +1,44 @@
 const colorPalette = document.querySelector('.color-palette');
+const output = document.querySelector('.output');
 
 colorPalette.addEventListener('click', selectColor);
 
+// This is where delegation «magic» happens
 function selectColor(event) {
-  console.log(event.target);
-  const selectedColor = event.target.dataset.color;
-}
-
-function selectColor(event) {
+  console.log(event);
   if (event.target.nodeName !== 'BUTTON') {
-    return; // користувач клікнув між кнопками
+    return;
   }
 
-  const selectedColor = event.target.dataset.color; // користувач клікнув на кнопку і ми маємо доступ до її атрибутів
+  const selectedColor = event.target.dataset.color;
+  output.textContent = `Selected color: ${selectedColor}`;
+  output.style.color = selectedColor;
+}
+
+// Some helper functions to render palette items
+createPaletteItems();
+
+function createPaletteItems() {
+  const items = [];
+  for (let i = 0; i < 60; i++) {
+    const color = getRandomHexColor();
+    const item = document.createElement('button');
+    item.type = 'button';
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.classList.add('item');
+    items.push(item);
+  }
+  colorPalette.append(...items);
+}
+
+function getRandomHexColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
 }

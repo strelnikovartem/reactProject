c7e0bc9e626361835ee74aedf88089575bfc8ba4
Artem Strelnikov <piramida1237@gmail.com>
2025-08-05 12:32:49 +0200

    wwsap

--- js/react.js
+++ js/react.js
@@ -2,14 +2,15 @@ const parent = document.querySelector('#parent');
 const child = document.querySelector('#child');
 const descendant = document.querySelector('#descendant');
 
-parent.addEventListener('click', () => {
+parent.addEventListener('click', e => {
+  e.stopPropagation();
   alert(
     'Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!'
   );
 });
 
 child.addEventListener('click', evt => {
-  evt.stopPropagation();
+  // evt.stopPropagation();
   alert(
     'Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!'
   );

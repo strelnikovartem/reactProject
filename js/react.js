dfa5ca718db8a4c49b56846b8e09425eb30987ed
Artem Strelnikov <piramida1237@gmail.com>
2025-08-05 12:26:13 +0200

    add_cildren.currentTarget

--- js/react.js
+++ js/react.js
@@ -1,15 +1,6 @@
 const parent = document.querySelector('#parent');
-const child = document.querySelector('#child');
-const descendant = document.querySelector('#descendant');
 
-parent.addEventListener('click', () => {
-  console.log('Parent click handler');
-});
-
-child.addEventListener('click', () => {
-  console.log('Child click handler');
-});
-
-descendant.addEventListener('click', () => {
-  console.log('Descendant click handler');
+parent.addEventListener('click', event => {
+  console.log('event.target: ', event.target);
+  console.log('event.currentTarget: ', event.currentTarget);
 });

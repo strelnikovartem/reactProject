53779dd2f1e4b340485dd6d15b7f2ecb9a4fbe05
Artem Strelnikov <piramida1237@gmail.com>
2025-08-05 19:36:12 +0200

    add_css

--- js/react.js
+++ js/react.js
@@ -6,3 +6,11 @@ function selectColor(event) {
   console.log(event.target);
   const selectedColor = event.target.dataset.color;
 }
+
+function selectColor(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return; // користувач клікнув між кнопками
+  }
+
+  const selectedColor = event.target.dataset.color; // користувач клікнув на кнопку і ми маємо доступ до її атрибутів
+}

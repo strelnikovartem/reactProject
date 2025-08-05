6f8f172902aa628fbadba31602f263fa6684b58c
Artem Strelnikov <piramida1237@gmail.com>
2025-08-05 19:28:56 +0200

    sxdsxdc

--- js/react.js
+++ js/react.js
@@ -1,22 +1,5 @@
-const parent = document.querySelector('#parent');
-const child = document.querySelector('#child');
-const descendant = document.querySelector('#descendant');
+const box = document.querySelector('div.box');
 
-parent.addEventListener('click', e => {
-  e.stopPropagation();
-  alert(
-    'Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!'
-  );
-});
-
-child.addEventListener('click', evt => {
-  // evt.stopPropagation();
-  alert(
-    'Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!'
-  );
-});
-
-descendant.addEventListener('click', event => {
-  // event.stopPropagation();
-  alert('Descendant click handler');
+box.addEventListener('click', function (event) {
+  console.log(event.target);
 });

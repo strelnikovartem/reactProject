f3d6c23b8282a6fee748cbe3dc40476058ef7d8f
Artem Strelnikov <piramida1237@gmail.com>
2025-08-06 16:37:59 +0200

    add_js-function

--- js/boxes.js
+++ js/boxes.js
@@ -1,1 +1,10 @@
 const container = document.querySelector('.container');
+
+[...container.children].forEach(box => {
+  box.addEventListener('click', henleClick);
+});
+
+function henleClick(evt) {
+  const color = evt.target.dataset.color;
+  console.log(color);
+}

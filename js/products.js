ea9d18bf8a4ae72dc1aac0e222f6a65267b7e86a
Artem Strelnikov <piramida1237@gmail.com>
2025-08-06 17:56:49 +0200

    task-products

---
+++ js/products.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    id: 1,
+    img: 'https://www.vodafone.ua/shop/media/wysiwyg/novosti/Capture_1_large.JPG',
+    name: 'Monitor',
+    price: 3000,
+    description: '23-inch monitor with Full HD resolution.',
+  },
+  {
+    id: 2,
+    img: 'https://cdn.27.ua/799/66/39/6841913_1.jpeg',
+    name: 'Smartphone',
+    price: 8000,
+    description: 'Equipped with a triple camera and a multi-core processor.',
+  },
+  {
+    id: 3,
+    img: 'https://cdn.27.ua/799/b6/16/4371990_1.jpeg',
+    name: 'Tablet',
+    price: 12000,
+    description: '10-inch tablet with high performance and a Retina display.',
+  },
+];

a57d9e82dfeddfa9a8a24c6348b455bf2daa8fa3
Artem Strelnikov <piramida1237@gmail.com>
2025-08-20 19:30:49 +0200

    feedback-form

--- js/react.js
+++
@@ -1,8 +0,0 @@
-const LS_KEY = 'array of names';
-const names = ['Ruslan', 'Lena', 'Artem'];
-
-localStorage.setItem(LS_KEY, JSON.stringify(names));
-
-const value = localStorage.getItem(LS_KEY);
-
-console.log(JSON.parse(value));

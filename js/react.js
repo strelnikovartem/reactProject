00eae98026ca6bbbb588de1e7fdd29184508edb1
Artem Strelnikov <piramida1237@gmail.com>
2025-08-20 19:56:14 +0200

    fuckk

--- js/react.js
+++ js/react.js
@@ -2,3 +2,10 @@ const STORAGE_KEY = 'feedback-msg';
 
 const form = document.querySelector('.feedback-form');
 const textarea = document.querySelector('textarea');
+
+textarea.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  const message = event.target;
+  console.log(message);
+}

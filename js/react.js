f75a5e9380c7c8fe8477e29638a8effe9f9079de
Artem Strelnikov <piramida1237@gmail.com>
2025-08-20 19:06:18 +0200

    dack1

--- js/react.js
+++ js/react.js
@@ -2,3 +2,7 @@ const LS_KEY = 'array of names';
 const names = ['Ruslan', 'Lena', 'Artem'];
 
 localStorage.setItem(LS_KEY, names);
+
+const value = localStorage.getItem(LS_KEY);
+
+console.log(value);
